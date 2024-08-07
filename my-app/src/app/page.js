eccb997b5844c1a76a1eeaca7d6e8300bdd29141
tgondil <tgondil@purdue.edu>
2024-08-07 08:25:00 +0530

    initial commit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <>
+    <div className="h-screen bg-black">
+      <div id="navbar" className="h-1/6 bg-white flex items-center">
+        <img src="/Logo.png" className="h-12 w-42"></img>
+      </div>
+      
+    </div>
+    </>
+  );
+}
